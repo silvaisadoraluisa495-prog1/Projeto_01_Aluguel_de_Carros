a87ddf2dc3c63140642e6675a061f83830e73d66
silvaisadoraluisa495-prog1 <silvaisadoraluisa495@gmail.com>
2026-05-21 11:57:33 -0300

    Add files via upload
    Primeira versão do site

--- app.py
+++ app.py
@@ -1,60 +1,61 @@
 import streamlit as st
+st.title('LS Drive - Aluguel de carros')
+st.sidebar.title('Escolha o seu modelo')
+st.sidebar.image('logo.png')
 
-# python -m streamlit run app.py
+carros = ['BMW', 'Mustang', 'Porsche', 'Jaguar', 'Chevrolet','Fiat', 'Corsa 97' ,'Civic' ,'Parati' ] 
 
-# ------------------------------------------------- Sidebar
-
-st.sidebar.image("logo.png")
-st.sidebar.title('Mateus Motors')
-
-
-carros = ['BMW','Mustang', 'Porsche', 'Fusca', 'Toro']
-
-opcao = st.sidebar.selectbox('Escolha o carro que foi alugado', carros)
-
-
-
-# ----------------------------------------------- Principal 
-st.title('Mateus motors - Aluguel de Carros')
-
-st.image(f'{opcao}.png')
-st.markdown(f'## Você alugou o modelo: {opcao}')
+opçao = st.sidebar.selectbox('Escolha o carro que foi alugado' , carros)
+# st.sidebar.image('BMW.png')
+# st.sidebar.image('Mustang.png')
+# st.sidebar.image('Porsche.png')
+# st.sidebar.image('Jaguar.png')
+# st.sidebar.image('Chevrolet.png')
+# st.sidebar.image('Fiat.png')
+# st.sidebar.image('Corsa 97.png')
+# st.sidebar.image('Civic.png')
+# st.sidebar.image('Parati.png')
+st.image(f'{opçao}.png')
+st.markdown(f'## Você alugou o modelo: {opçao}')
 st.markdown('---')
 
-dias = st.text_input(f'Por quantos dias o {opcao} foi alugado?')
-km = st.text_input(f'Quantos km você rodou com o {opcao}?')
+dias = st.text_input(f'Por quantos dias o {opçao} foi alugado?')
+km = st.text_input(f'Quantos km você rodou com o {opçao}?')
 
-if opcao == 'BMW':
-    diaria = 450
-
-elif opcao == 'Mustang':
+if opçao =='BMW':
     diaria = 500
 
-elif opcao == 'Porsche':
-    diaria = 300
+elif opçao == 'Mustang':
+    diaria = 460
 
-elif opcao == 'Fusca':
-    diaria = 250
-
-elif opcao == 'Toro':
+elif opçao == 'Porsche':
     diaria = 550
 
+elif opçao == 'Jaguar':
+    diaria = 550
 
+elif opçao == 'Chevrolet':
+    diaria = 350
 
+elif opçao == 'Fiat':
+    diaria = 350
 
+elif opçao == 'Corsa 97':
+    diaria = 300
+
+elif opçao == 'Civic':
+    diaria = 400
+
+elif opçao == 'Parati':
+    diaria = 250
 
 if st.button('Calcular'):
     dias = int(dias)
     km = float(km)
 
     total_dias = dias * diaria
-    total_km = km * 0.15
-    aluguel_total = total_dias+total_km
-
-    st.warning(f'Você alugou o {opcao} por {dias} dias e rodou {km}km. O valor total a pagar é R${aluguel_total:.2f}')
+    total_km = km * 0.50
+    aluguel_total  = total_dias+total_km
 
 
-
-
-
-
+st.warning(f'Voce alugou o {opçao} por {dias} dias e rodou {km} km. O valor total a pagar é R$ {aluguel_total:.2f}')
